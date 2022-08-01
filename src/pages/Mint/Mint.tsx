1102caaa254001a97d52c62351c231ab67df71de
Artem Lagunov <lagunov.artamon@gmail.com>
2022-08-01 16:13:49 +0300

    created base layout for mint page

--- src/pages/Mint/Mint.tsx
+++ src/pages/Mint/Mint.tsx
@@ -1,7 +1,19 @@
 import { Page } from 'components';
 
+import styles from './Mint.module.scss';
+
 function Mint() {
-  return <Page>mint</Page>;
+  return (
+    <Page>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Example nft</h1>
+        <div className={styles.nftWrapper}>
+          <div className={styles.nft}>nft</div>
+          <div className={styles.subtitle}>Already minted</div>
+        </div>
+      </div>
+    </Page>
+  );
 }
 
 export default Mint;
