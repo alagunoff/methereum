958ecfe549b60c7ebbc08391d089fe7a355aca36
Artem Lagunov <lagunov.artamon@gmail.com>
2022-08-01 15:00:18 +0300

    added displaying user's wallet

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
+
+import { selectUserWallet } from 'store/user';
 import { List } from 'components/uiKit';
 
 import { LINKS } from './constants';
 import styles from './Header.module.scss';
 
 function Header() {
+  const userWallet = useSelector(selectUserWallet);
+
   return (
     <header className={styles.container}>
+      <div className={styles.wallet}>{userWallet}</div>
       <nav className={styles.navigation}>
         <List
           items={LINKS.map(({ text, href }) => (
