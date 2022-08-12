149338307f46710d29b1798b2f95fe96d7144532
Artem Lagunov <lagunov.artamon@gmail.com>
2022-08-12 10:45:05 +0300

    added tokens list to swap widget

--- src/components/ConvertCurrencyModal/ConvertCurrencyModal.tsx
+++ src/components/ConvertCurrencyModal/ConvertCurrencyModal.tsx
@@ -4,6 +4,7 @@ import { SwapWidget } from '@uniswap/widgets';
 import { Modal } from 'components/uiKit';
 
 import { IProps } from './types';
+import { tokens } from './data';
 import styles from './ConvertCurrencyModal.module.scss';
 
 function ConvertCurrencyModal({ onClose }: IProps) {
@@ -14,7 +15,7 @@ function ConvertCurrencyModal({ onClose }: IProps) {
       <h2 className={styles.title}>Add funds</h2>
       <div className={styles.subtitle}>Easily convert between currencies</div>
       <div className={styles.widget}>
-        <SwapWidget provider={library} hideConnectionUI />
+        <SwapWidget provider={library} hideConnectionUI tokenList={tokens} />
       </div>
     </Modal>
   );
---
+++ src/components/ConvertCurrencyModal/data.ts
@@ -0,0 +1,15 @@
+import { Goerli } from '@usedapp/core';
+
+const tokens = [
+  {
+    address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    symbol: 'WETH',
+    name: 'Wrapped Ether',
+    chainId: Goerli.chainId,
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6/logo.png',
+  },
+];
+
+export { tokens };
