fe522b81814d6d5214461d371b9ed9c17a661224
Artem Lagunov <lagunov.artamon@gmail.com>
2022-08-17 18:44:06 +0300

    changed chainId source for uniswap widget

--- src/components/ConvertCurrencyModal/data.ts
+++ src/components/ConvertCurrencyModal/data.ts
@@ -1,11 +1,11 @@
-import { Goerli } from '@usedapp/core';
+import { goerli } from 'wagmi/chains';
 
 const tokens = [
   {
     address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
     symbol: 'WETH',
     name: 'Wrapped Ether',
-    chainId: Goerli.chainId,
+    chainId: goerli.id,
     decimals: 18,
     logoURI:
       'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6/logo.png',
